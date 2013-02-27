75d175d4c7a01d7baabc7b1dec28255579fef673
Elias K. Obeid <eobeid11@student.aau.dk>
2013-02-27 12:54:04 +0100

    parser

---
+++ program/Parser/src/Parser.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class Parser {
+
+	/**
+	 * @param args
+	 */
+	
+	private LinkedList<Token> tokens;
+	private Token currentToken;
+	private Token nextToken;
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
