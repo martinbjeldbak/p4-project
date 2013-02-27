86da7c58989777dc7541df082dd43cdea176bd3c
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2013-02-27 10:36:33 +0100

    Init Simulator java project

--- program/Simulator/src/start/HelloWorld.java
+++ program/Simulator/src/start/HelloWorld.java
@@ -1,6 +1,30 @@
 
-import java.awt.*;
+package start;
+
+import javax.swing.*;
 
 public class HelloWorld {
-
+	private static void createAndShowGUI() {
+		// Create and set up window
+		JFrame frame = new JFrame("HelloWorld");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Add label
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+		
+		// Display the window
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 }
