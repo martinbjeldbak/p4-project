0bd76f2d591d5c41d8055740f2b9c6f6eb8a5ab7
spillerrec <spillerrec@hotmail.com>
2013-03-01 11:49:51 +0100

    ADD: chess came moved from docs and updated syntax ADD: syntax highlighting for Notepad++

---
+++ program/notepad++lexer.py
@@ -0,0 +1,61 @@
+<NotepadPlus>
+    <UserLang name="ludo" ext="game" udlVersion="2.0">
+        <Settings>
+            <Global caseIgnored="no" allowFoldOfComments="no" forceLineCommentsAtBOL="no" foldCompact="no" />
+            <Prefix Keywords1="no" Keywords2="no" Keywords3="yes" Keywords4="no" Keywords5="no" Keywords6="yes" Keywords7="no" Keywords8="no" />
+        </Settings>
+        <KeywordLists>
+            <Keywords name="Comments" id="0">00 01 02 03// 04!</Keywords>
+            <Keywords name="Numbers, additional" id="1"></Keywords>
+            <Keywords name="Numbers, prefixes" id="2"></Keywords>
+            <Keywords name="Numbers, extras with prefixes" id="3"></Keywords>
+            <Keywords name="Numbers, suffixes" id="4"></Keywords>
+            <Keywords name="Operators1" id="5"> ! * + / |</Keywords>
+            <Keywords name="Operators2" id="6"></Keywords>
+            <Keywords name="Folders in code1, open" id="7">{ ( [</Keywords>
+            <Keywords name="Folders in code1, middle" id="8"></Keywords>
+            <Keywords name="Folders in code1, close" id="9">} ) ]</Keywords>
+            <Keywords name="Folders in code2, open" id="10"></Keywords>
+            <Keywords name="Folders in code2, middle" id="11"></Keywords>
+            <Keywords name="Folders in code2, close" id="12"></Keywords>
+            <Keywords name="Folders in comment, open" id="13"></Keywords>
+            <Keywords name="Folders in comment, middle" id="14"></Keywords>
+            <Keywords name="Folders in comment, close" id="15"></Keywords>
+            <Keywords name="Keywords1" id="16">game&#x000D;&#x000A;title&#x000D;&#x000A;players&#x000D;&#x000A;turnOrder&#x000D;&#x000A;board&#x000D;&#x000A;grid&#x000D;&#x000A;width&#x000D;&#x000A;height&#x000D;&#x000A;setyp&#x000D;&#x000A;piece&#x000D;&#x000A;name&#x000D;&#x000A;setup&#x000D;&#x000A;if&#x000D;&#x000A;then&#x000D;&#x000A;else&#x000D;&#x000A;possibleMoves&#x000D;&#x000A;winCondition&#x000D;&#x000A;define&#x000D;&#x000A;</Keywords>
+            <Keywords name="Keywords2" id="17">A1 A2 A3 A4 A5 A6 A7 A8 A9 A10 A11 A12 A13 A14 A15 A16 A17 A18 A19 A20 &#x000D;&#x000A;B1 B2 B3 B4 B5 B6 B7 B8 B9 B10 B11 B12 B13 B14 B15 B16 B17 B18 B19 B20 &#x000D;&#x000A;C1 C2 C3 C4 C5 C6 C7 C8 C9 C10 C11 C12 C13 C14 C15 C16 C17 C18 C19 C20 &#x000D;&#x000A;D1 D2 D3 D4 D5 D6 D7 D8 D9 D10 D11 D12 D13 D14 D15 D16 D17 D18 D19 D20 &#x000D;&#x000A;E1 E2 E3 E4 E5 E6 E7 E8 E9 E10 E11 E12 E13 E14 E15 E16 E17 E18 E19 E20 &#x000D;&#x000A;F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15 F16 F17 F18 F19 F20 &#x000D;&#x000A;G1 G2 G3 G4 G5 G6 G7 G8 G9 G10 G11 G12 G13 G14 G15 G16 G17 G18 G19 G20 &#x000D;&#x000A;H1 H2 H3 H4 H5 H6 H7 H8 H9 H10 H11 H12 H13 H14 H15 H16 H17 H18 H19 H20 &#x000D;&#x000A;I1 I2 I3 I4 I5 I6 I7 I8 I9 I10 I11 I12 I13 I14 I15 I16 I17 I18 I19 I20 &#x000D;&#x000A;J1 J2 J3 J4 J5 J6 J7 J8 J9 J10 J11 J12 J13 J14 J15 J16 J17 J18 J19 J20 &#x000D;&#x000A;K1 K2 K3 K4 K5 K6 K7 K8 K9 K10 K11 K12 K13 K14 K15 K16 K17 K18 K19 K20 &#x000D;&#x000A;L1 L2 L3 L4 L5 L6 L7 L8 L9 L10 L11 L12 L13 L14 L15 L16 L17 L18 L19 L20 &#x000D;&#x000A;M1 M2 M3 M4 M5 M6 M7 M8 M9 M10 M11 M12 M13 M14 M15 M16 M17 M18 M19 M20 &#x000D;&#x000A;N1 N2 N3 N4 N5 N6 N7 N8 N9 N10 N11 N12 N13 N14 N15 N16 N17 N18 N19 N20 &#x000D;&#x000A;O1 O2 O3 O4 O5 O6 O7 O8 O9 O10 O11 O12 O13 O14 O15 O16 O17 O18 O19 O20 &#x000D;&#x000A;P1 P2 P3 P4 P5 P6 P7 P8 P9 P10 P11 P12 P13 P14 P15 P16 P17 P18 P19 P20 &#x000D;&#x000A;Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14 Q15 Q16 Q17 Q18 Q19 Q20 </Keywords>
+            <Keywords name="Keywords3" id="18">Q W E R T Y U I O P&#x000D;&#x000A;A S D F G H J K L&#x000D;&#x000A;Z X C V B N M</Keywords>
+            <Keywords name="Keywords4" id="19">this&#x000D;&#x000A;friend&#x000D;&#x000A;foe&#x000D;&#x000A;empty&#x000D;&#x000A;</Keywords>
+            <Keywords name="Keywords5" id="20">e n s w&#x000D;&#x000A;ne nw se sw</Keywords>
+            <Keywords name="Keywords6" id="21">$</Keywords>
+            <Keywords name="Keywords7" id="22"></Keywords>
+            <Keywords name="Keywords8" id="23"></Keywords>
+            <Keywords name="Delimiters" id="24">00 01 02 03&quot; 04 05&quot; 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23</Keywords>
+        </KeywordLists>
+        <Styles>
+            <WordsStyle name="DEFAULT" styleID="0" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="COMMENTS" styleID="1" fgColor="008000" bgColor="FFFFFF" fontStyle="2" nesting="0" />
+            <WordsStyle name="LINE COMMENTS" styleID="2" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="NUMBERS" styleID="3" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="KEYWORDS1" styleID="4" fgColor="0080FF" bgColor="FFFFFF" fontStyle="1" nesting="0" />
+            <WordsStyle name="KEYWORDS2" styleID="5" fgColor="8080C0" bgColor="FFFFFF" fontStyle="1" nesting="0" />
+            <WordsStyle name="KEYWORDS3" styleID="6" fgColor="FF8000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="KEYWORDS4" styleID="7" fgColor="8080FF" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="KEYWORDS5" styleID="8" fgColor="FF8000" bgColor="FFFFFF" fontStyle="1" nesting="0" />
+            <WordsStyle name="KEYWORDS6" styleID="9" fgColor="008000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="KEYWORDS7" styleID="10" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="KEYWORDS8" styleID="11" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="OPERATORS" styleID="12" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="FOLDER IN CODE1" styleID="13" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="FOLDER IN CODE2" styleID="14" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="FOLDER IN COMMENT" styleID="15" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="DELIMITERS1" styleID="16" fgColor="000000" bgColor="FFFFFF" fontStyle="2" nesting="0" />
+            <WordsStyle name="DELIMITERS2" styleID="17" fgColor="808080" bgColor="FFFFFF" fontStyle="2" nesting="0" />
+            <WordsStyle name="DELIMITERS3" styleID="18" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="DELIMITERS4" styleID="19" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="DELIMITERS5" styleID="20" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="DELIMITERS6" styleID="21" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="DELIMITERS7" styleID="22" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+            <WordsStyle name="DELIMITERS8" styleID="23" fgColor="000000" bgColor="FFFFFF" fontStyle="0" nesting="0" />
+        </Styles>
+    </UserLang>
+</NotepadPlus>
