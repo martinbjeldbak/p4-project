3ebbbcaed6afb2f0e49fc647a5471fb4032194f6
Elias K. Obeid <eobeid11@student.aau.dk>
2013-03-03 20:10:25 +0100

    fejl i skanner rettet.

--- program/Scanner/src/dk/aau/cs/d402f13/scanner/Scanner.java
+++ program/Scanner/src/dk/aau/cs/d402f13/scanner/Scanner.java
@@ -8,92 +8,92 @@ import java.io.UnsupportedEncodingException;
 
 import dk.aau.cs.d402f13.scanner.Token.Type;
 
-
 @SuppressWarnings("unused")
 public class Scanner {
   public static final String whitespace = " \t\r\n";
   public static final String operators = "!&*+-=>?(){}[]/|";
   public int offset, line;
 
-
   private InputStreamReader input;
   private int nextChar = -1;
-  
+
   public Scanner(InputStream input) throws UnsupportedEncodingException {
-    this.line = 1;  //keeps track of line number and offset so when meeting an error, users can see on which line and offset they messed up
+    this.line = 1; // keeps track of line number and offset so when meeting an
+                   // error, users can see on which line and offset they messed
+                   // up
     this.offset = 1;
     this.input = new InputStreamReader(input, "UTF-8");
     pop();
   }
- 
+
   public char peek() {
-    return (char)nextChar;
+    return (char) nextChar;
   }
-  
+
   public char pop() {
     int value = nextChar;
-    this.offset++; 
-    if (isEol())
-    {
-     this.line++;
-     this.offset = 1;
+    this.offset++;
+    if (isEol()) {
+      this.line++;
+      this.offset = 1;
     }
-      
+
     try {
       nextChar = input.read();
     }
-    catch (IOException exception) {
-      
-    }
-    return (char)value;
+    catch (IOException exception) {}
+    return (char) value;
   }
 
   public boolean isEol() {
     return peek() == '\n';
   }
-  
+
   public boolean isEof() {
-    return (int)peek() == 65535;
+    return (int) peek() == 65535;
   }
-  
+
   public boolean isWhitespace() {
     return whitespace.indexOf(peek()) != -1;
   }
-  
+
   public boolean isDigit() {
     char c = peek();
     return c >= '0' && c <= '9';
   }
+
   public boolean isOperator() {
     return operators.indexOf(peek()) != -1;
   }
+
   public boolean isUppercase() {
     char c = peek();
     return c >= 'A' && c <= 'Z';
   }
+
   public boolean isLowercase() {
     char c = peek();
     return c >= 'a' && c <= 'z';
   }
+
   public boolean isAlphacharacter() {
     char c = peek();
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
   }
-  
+
   public Token scanKeyword() throws Exception {
     String value = "";
     while (isAlphacharacter()) {
       value += pop();
     }
-    switch (value)
-    {
-    case "if": 
+    switch (value) {
+    case "if":
       return new Token(Type.IF, line, offset);
     case "then":
       return new Token(Type.THEN, line, offset);
     case "else":
       return new Token(Type.ELSE, line, offset);
-    //KEYWORD  
+      // KEYWORD
     case "game":
       return new Token(Type.GAME, line, offset);
     case "piece":
@@ -128,19 +128,19 @@ public class Scanner {
       return new Token(Type.KEYWORD, "win_condition", line, offset);
     case "tie_condition":
       return new Token(Type.KEYWORD, "tie_condition", line, offset);
-    //Operators
+      // Operators
     case "and":
       return new Token(Type.OPERATOR, "and", line, offset);
     case "or":
       return new Token(Type.OPERATOR, "or", line, offset);
-    //Pattern keywords
+      // Pattern keywords
     case "foe":
       return new Token(Type.PATTERN_KEYWORD, "foe", line, offset);
     case "friend":
       return new Token(Type.PATTERN_KEYWORD, "friend", line, offset);
     case "empty":
       return new Token(Type.PATTERN_KEYWORD, "empty", line, offset);
-    //Direction
+      // Direction
     case "ne":
       return new Token(Type.DIR_LIT, "ne", line, offset);
     case "nw":
@@ -157,95 +157,101 @@ public class Scanner {
       return new Token(Type.DIR_LIT, "e", line, offset);
     case "w":
       return new Token(Type.DIR_LIT, "w", line, offset);
-    
+
     case "define":
       return new Token(Type.DEFINE, line, offset);
     }
     return new Token(Type.FUNCTION, value, line, offset);
   }
+
   public Token scanId() {
-    //Like Noughts or Crosses
-     String value = "";
-     while (isAlphacharacter()) {
-       value += pop();
-     }
-     return new Token(Type.ID, value, line, offset);
-   }
-  public Token scanUppercase() {
-    //Can be ID or Coordinate, e.g. Noughts or A3
-     String value = "";
-     while (isAlphacharacter()) {
-       value += pop();
-     }
-     if (isDigit())  //if digit comes after the alphacharacters, it must be a coordinate, else an identifier
-     {
-       while (isDigit())
-         value += pop();
-       return new Token(Type.COORD_LIT, value, line, offset);
-     }
-     else
-       return new Token(Type.ID, value, line, offset);
-       
-   }
- public Token scanVar() {
-   //called when token starts with $
+    // Like Noughts or Crosses
     String value = "";
-    pop(); //remove initial $
+    while (isAlphacharacter()) {
+      value += pop();
+    }
+    return new Token(Type.ID, value, line, offset);
+  }
+
+  public Token scanUppercase() {
+    // Can be ID or Coordinate, e.g. Noughts or A3
+    String value = "";
+    while (isAlphacharacter()) {
+      value += pop();
+    }
+    if (isDigit()) // if digit comes after the alphacharacters, it must be a
+                   // coordinate, else an identifier
+    {
+      while (isDigit()){
+        value += pop();
+      }
+      return new Token(Type.COORD_LIT, value, line, offset);
+    }
+    else return new Token(Type.ID, value, line, offset);
+
+  }
+
+  public Token scanVar() {
+    // called when token starts with $
+    String value = "";
+    pop(); // remove initial $
     while (isAlphacharacter()) {
       value += pop();
     }
     return new Token(Type.VAR, value, line, offset);
   }
-  
+
   public Token scanOperator() throws Exception {
     String value = "";
-      while (isOperator()) {
-        value += pop();
-        if (value.length() == 2 && value.equals("//")) {
-          while (!isEol()) {
-            pop();
-          }
-          return scan();
+    while (isOperator()) {
+      value += pop();
+      if (value.length() == 2 && value.equals("//")) {
+        while (!isEol()) {
+          pop();
         }
-        switch (value) {    //these operators are unambiguous
-        case "[":
-          return new Token(Type.LBRACKET, line, offset);
-        case "]":
-          return new Token(Type.RBRACKET, line, offset);
-        case "{":
-          return new Token(Type.LBRACE, line, offset);
-        case "}":
-          return new Token(Type.RBRACE, line, offset);
-        case "(":
-          return new Token(Type.LPAREN, line, offset);
-        case ")":
-          return new Token(Type.RPAREN, line, offset);
-        case "!":
-          return new Token(Type.RPAREN, line, offset);
-        case "+":
-          return new Token(Type.PATTERN_OPERATOR, "plus_op", line, offset);
-        case "-":
-          return new Token(Type.PATTERN_OPERATOR, "minus_op", line, offset);
-        case "*":
-          return new Token(Type.PATTERN_OPERATOR, "mult_op", line, offset);
-        case "?":
-          return new Token(Type.PATTERN_OPERATOR, "quest_op", line, offset);
-        case "/":
-          return new Token(Type.PATTERNOP, line, offset);
-        case "|":
-          return new Token(Type.OROP, line, offset);
-        case "#":
-          return new Token(Type.LAMBDABEGIN, line, offset);
+        return scan();
       }
+      switch (value) { // these operators are unambiguous
+      case "[":
+        return new Token(Type.LBRACKET, line, offset);
+      case "]":
+        return new Token(Type.RBRACKET, line, offset);
+      case "{":
+        return new Token(Type.LBRACE, line, offset);
+      case "}":
+        return new Token(Type.RBRACE, line, offset);
+      case "(":
+        return new Token(Type.LPAREN, line, offset);
+      case ")":
+        return new Token(Type.RPAREN, line, offset);
+      case "!":
+        return new Token(Type.NOTOP, line, offset);
+      case "+":
+        return new Token(Type.PATTERN_OPERATOR, "plus_op", line, offset);
+      case "-":
+        return new Token(Type.PATTERN_OPERATOR, "minus_op", line, offset);
+      case "*":
+        return new Token(Type.PATTERN_OPERATOR, "mult_op", line, offset);
+      case "?":
+        return new Token(Type.PATTERN_OPERATOR, "quest_op", line, offset);
+      case "/":
+        return new Token(Type.PATTERNOP, line, offset);
+      case "|":
+        return new Token(Type.OROP, line, offset);
+        //Hvad er det for en OR operator?! Har vi to forskellige OR?
+      case "#":
+        return new Token(Type.LAMBDABEGIN, line, offset);
+      }
+      //Er der nogen grund til at have denne i en switch for sig?
+      //Vi har ingen assign (=) operator.
       switch (value) {
-        case "=>":
-          return new Token(Type.LAMBDAOP, line, offset);
+      case "=>":
+        return new Token(Type.LAMBDAOP, line, offset);
       }
-     
     }
     throw new Exception("Undefined token " + value);
- }
-  
+  }
+
   public Token scanNumeric() {
     String value = "";
     while (isDigit()) {
@@ -253,7 +259,7 @@ public class Scanner {
     }
     return new Token(Type.INT_LIT, value, line, offset);
   }
-  
+
   public Token scanString() {
     String value = "";
     pop();
@@ -266,43 +272,30 @@ public class Scanner {
     }
     return new Token(Type.STRING_LIT, value, line, offset);
   }
-  
+
   public Token scan() throws Exception {
     while (isWhitespace()) {
       pop();
     }
-    if (isEof()) {
-      return new Token(Type.EOF, line, offset);
-    }
-    if (isDigit()) {
-      return scanNumeric();
-    }
-    if (isUppercase()) {
-      return scanUppercase();
-    }
-    if (isOperator()) {
-      return scanOperator();
-    }
-    if (isLowercase()){
-      return scanKeyword();
-    }
-    if (peek() == '"'){
-      return scanString();
-    }
-    if (peek() == '$'){
-      return scanVar();
-    }
-    
-    throw new Exception("Could not find a valid token starting with char " + peek());
+    if (isEof()) { return new Token(Type.EOF, line, offset); }
+    if (isDigit()) { return scanNumeric(); }
+    if (isUppercase()) { return scanUppercase(); }
+    if (isOperator()) { return scanOperator(); }
+    if (isLowercase()) { return scanKeyword(); }
+    if (peek() == '"') { return scanString(); }
+    if (peek() == '$') { return scanVar(); }
+
+    throw new Exception("Could not find a valid token starting with char "
+        + peek());
   }
-  
+
   public static void main(String[] args) throws Exception {
-  //  FileInputStream f = new FileInputStream("programExample1.txt");
+    // FileInputStream f = new FileInputStream("programExample1.txt");
     Scanner s = new Scanner(System.in);
     Token t;
     while ((t = s.scan()).type != Type.EOF) {
-      System.out.println("Read token: " + t.type + " (" + t.value + ") on line: " + t.line + ", offset: " + t.offset);
+      System.out.println("Read token: " + t.type + " (" + t.value
+          + ") on line: " + t.line + ", offset: " + t.offset);
     }
   }
-
 }
