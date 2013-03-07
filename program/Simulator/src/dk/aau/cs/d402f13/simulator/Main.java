70ecb71bdd81436575e58a5eda7973f98bab3f2b
Martin Madsen <martin2madsen@gmail.com>
2013-03-07 23:21:33 +0100

    Rebase simulator to use lwjgl. Delete all previous classes/libraries and started over

--- program/Simulator/src/dk/aau/cs/d402f13/simulator/Main.java
+++ program/Simulator/src/dk/aau/cs/d402f13/simulator/Main.java
@@ -2,24 +2,22 @@ package dk.aau.cs.d402f13.simulator;
 
 import java.awt.EventQueue;
 
-public class Main {
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+import dk.aau.cs.d402f13.simulator.SimulatorGUI;
 
-	  System.setProperty("apple.laf.useScreenMenuBar", "true");
-	  
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-				  System.setProperty("apple.laf.useScreenMenuBar", "true");
-					SimulatorGUI frame = new SimulatorGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+public class Main {
+
+  public Main() {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+          try {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            SimulatorGUI frame = new SimulatorGUI();
+            frame.setVisible(true);
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+      }
+  });
+  }
+
 }
