4290cbf1f20138208bdbbcb2fe4246ec51bbc5c4
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2013-03-07 23:21:33 +0100

    Rebase simulator to use lwjgl. Delete all previous classes/libraries and started over

--- program/Simulator/src/dk/aau/cs/d402f13/simulator/Board.java
+++ program/Simulator/src/dk/aau/cs/d402f13/simulator/Board.java
@@ -1,40 +1,69 @@
 package dk.aau.cs.d402f13.simulator;
 
-import java.awt.GridLayout;
+import java.awt.Graphics;
 import java.awt.LayoutManager;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.text.FieldPosition;
+import java.awt.geom.Rectangle2D;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import dk.aau.cs.d402f13.simulator.actions.FileBrowserAction;
-
 public class Board extends JPanel {
-  Field[][] field;
 
   public Board() {
-  }
-  
-  public void setDimensions(int height, int width) {
-    field = new Field[height][width];
-    
-    for(int i = 0; i < height; i++) {
-      for(int j = 0; j < width; j++) {
-        field[i][j] = new Field(i, j);
+    // TODO Auto-generated constructor stub
+    addMouseListener(new MouseListener() {
+      
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
         
-        // Add the field to game board
-        this.add(field[i][j]);
       }
-    }
-    
-    this.setLayout(new GridLayout(height, width, 2, 2));
+      
+      @Override
+      public void mousePressed(MouseEvent e) {
+        System.out.println(e);
+        // TODO Auto-generated method stub
+      }
+      
+      @Override
+      public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+      
+      @Override
+      public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+      
+      @Override
+      public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+      }
+    });
   }
+
+  public Board(LayoutManager layout) {
+    super(layout);
+    // TODO Auto-generated constructor stub
+  }
+
+  public Board(boolean isDoubleBuffered) {
+    super(isDoubleBuffered);
+    // TODO Auto-generated constructor stub
+  }
+
+  public Board(LayoutManager layout, boolean isDoubleBuffered) {
+    super(layout, isDoubleBuffered);
+    // TODO Auto-generated constructor stub
+  }
+
   
-  public Field fieldAt(int x, int y) {
-    return field[x][y];
+  @Override
+  protected void paintComponent(Graphics g) {
   }
+
 }
--- program/Simulator/src/dk/aau/cs/d402f13/simulator/SimulatorGUI.java
+++ program/Simulator/src/dk/aau/cs/d402f13/simulator/SimulatorGUI.java
@@ -1,4 +1,5 @@
 package dk.aau.cs.d402f13.simulator;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -8,80 +9,77 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JScrollPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.IOException;
 
-import javax.swing.JButton;
-
-import dk.aau.cs.d402f13.simulator.actions.*;
-import javax.swing.JTextArea;
+import dk.aau.cs.d402f13.simulator.actions.FileBrowserAction;
 import javax.swing.JTextPane;
+
+import org.newdawn.slick.CanvasGameContainer;
+import org.newdawn.slick.SlickException;
 
 public class SimulatorGUI extends JFrame {
 
-	private JPanel contentPane;
+  private JPanel contentPane;
 
-	/**
-	 * Create the frame.
-	 */
-	public SimulatorGUI() {
-		setTitle("Simulator");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 505, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		
-		JMenuBar menuBar = new JMenuBar();
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          SimulatorGUI frame = new SimulatorGUI();
+          frame.setVisible(true);
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 
-		contentPane.add(menuBar, BorderLayout.NORTH);
-		
-		JMenu mnFile = new JMenu("File");
-		menuBar.add(mnFile);
-		
-		JMenuItem mntmBrowseGame = new JMenuItem("Import game file...");
-		mntmBrowseGame.addActionListener(new FileBrowserAction());
+  /**
+   * Create the frame.
+   */
+  public SimulatorGUI() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 450, 300);
+    
+    // MENU BAR
+    JMenuBar menuBar = new JMenuBar();
+    setJMenuBar(menuBar);
+    
+    JMenu mnFile = new JMenu("File");
+    menuBar.add(mnFile);
+    
+    JMenuItem mntmOpenFile = new JMenuItem("Open file...");
+    mntmOpenFile.addActionListener(new FileBrowserAction());
+    
+    mnFile.add(mntmOpenFile);
+    
+    // MAIN Content pane
+    contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    contentPane.setLayout(new BorderLayout(0, 0));
+    setContentPane(contentPane);
+  
+    // --Add game container to main content pane
+    BoardGame boardGame = new BoardGame();
+    CanvasGameContainer gameContainer;
+    try {
+      gameContainer = new CanvasGameContainer(boardGame);
+      contentPane.add(gameContainer, BorderLayout.CENTER);
+      
+      gameContainer.setSize(200, 200);
+      gameContainer.start();
+    } catch (SlickException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-		mnFile.add(mntmBrowseGame);
-		
-		Board board = new Board();
-		contentPane.add(board, BorderLayout.CENTER);
-		
-		JTextPane textPane = new JTextPane();
-		JScrollPane jsp = new JScrollPane(textPane);
-		textPane.setEditable(false);
-		textPane.setContentType("text/html");
-		textPane.setText("test");
-		contentPane.add(textPane, BorderLayout.EAST);
-		
-		board.setDimensions(2, 2);
+    // --Right JTextPane
+    JTextPane textPane = new JTextPane();
+    textPane.setContentType("text/html");
+    contentPane.add(textPane, BorderLayout.EAST);
+  }
 
-    board.field[0][0].setPicture("img/chess.svg");
-	}
-
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-	}
 }
