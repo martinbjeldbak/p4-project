e9c6fe1c01b57f725efef951404642339e0db3d1
Niels Poulsen <nielssonnich@gmail.com>
2013-03-11 12:12:49 +0100

    New utilities project

---
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/errors/ScannerError.java
@@ -0,0 +1,12 @@
+package dk.aau.cs.d402f13.utilities.errors;
+
+import dk.aau.cs.d402f13.utilities.Token;
+
+public class ScannerError extends SyntaxError {
+
+  public ScannerError(String msg, Token token) {
+    super(msg, token);
+    // TODO Auto-generated constructor stub
+  }
+
+}
