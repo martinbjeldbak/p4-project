ba54f23c9095bb33f5cfc5223d689df0a4d0586d
Martin Madsen <martin2madsen@gmail.com>
2013-03-26 11:50:38 +0100

    Init interpreter

---
+++ Interpreter/src/dk/aau/cs/d402f13/interpreter/Interpreter.java
@@ -0,0 +1,181 @@
+package dk.aau.cs.d402f13.interpreter;
+
+import dk.aau.cs.d402f13.utilities.ast.AstNode;
+import dk.aau.cs.d402f13.utilities.ast.Visitor;
+import dk.aau.cs.d402f13.utilities.errors.StandardError;
+
+public class Interpreter extends Visitor {
+
+  public Interpreter() {
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  protected Object visitAssignment(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitCoordLit(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitDecl(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitDeclStruct(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitDirLit(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitFunction(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitFuncCall(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitFuncDef(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitGameDecl(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitId(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitIfExpr(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitIntLit(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitKeyword(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitLambdaExpr(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitList(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitNotOperator(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitOperator(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitPattern(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitPatternKeyword(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitPatternMultiplier(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitPatternNot(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitPatternOperator(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitPatternOr(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitProgram(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitStringLit(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitThis(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitVar(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected Object visitVarlist(AstNode node) throws StandardError {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ Interpreter/src/dk/aau/cs/d402f13/interpreter/Main.java
@@ -0,0 +1,17 @@
+package dk.aau.cs.d402f13.interpreter;
+
+public class Main {
+
+  public Main() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
