9539e4c1b8e51d560439f614fe604834ad4ce05b
Martin Madsen <martin2madsen@gmail.com>
2013-04-08 14:00:40 +0200

    Add stuff to interpreter

---
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/errors/ArgumentError.java
@@ -0,0 +1,17 @@
+package dk.aau.cs.d402f13.utilities.errors;
+
+import dk.aau.cs.d402f13.utilities.ast.AstNode;
+
+public class ArgumentError extends StandardError {
+
+  public ArgumentError(String arg0, AstNode node) {
+    super(arg0, node);
+    // TODO Auto-generated constructor stub
+  }
+
+  public ArgumentError(String arg0) {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+}
