24c47065d54e935be5fd930e54949e677ea3d9eb
Elias K. Obeid <eobeid11@student.aau.dk>
2013-04-14 20:28:47 +0200

    Section about abstract node types has been rewritten.

--- program/Parser/src/dk/aau/cs/d402f13/parser/Parser.java
+++ program/Parser/src/dk/aau/cs/d402f13/parser/Parser.java
@@ -246,7 +246,7 @@ public class Parser {
   private AstNode constantDef() throws SyntaxError {
     AstNode node = astNode(Type.CONSTANT_DEF, "");
     expect(Token.Type.DEFINE);
-    expect(Token.Type.CONSTANT);
+    expect(Token.Type.CONSTANT); //constant eller function?
     if (lookAhead(Token.Type.LBRACKET)) {
       node.addChild(varList());
     }
@@ -259,7 +259,7 @@ public class Parser {
     AstNode node = astNode(Type.ABSTRACT_DEF, currentToken.value);
     expect(Token.Type.DEFINE);
     expect(Token.Type.ABSTRACT);
-    expect(Token.Type.CONSTANT);
+    expect(Token.Type.CONSTANT); //constant eller function?
     node.addChild(astNode(Type.CONSTANT, currentToken.value));
     if (lookAhead(Token.Type.LBRACKET)) {
       node.addChild(varList());
