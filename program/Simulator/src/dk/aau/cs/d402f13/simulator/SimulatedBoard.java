57c55f645335e21b0c27d784bc1c2ff91dc7e73a
Niels Poulsen <nielssonnich@gmail.com>
2013-04-19 12:16:12 +0200

    Merge branch 'master' of ssh://bitbucket.org/Acolarh/p4-project

--- program/Simulator/src/dk/aau/cs/d402f13/simulator/SimulatedBoard.java
+++ program/Simulator/src/dk/aau/cs/d402f13/simulator/SimulatedBoard.java
@@ -3,12 +3,16 @@ package dk.aau.cs.d402f13.simulator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimulatedBoard {
+import dk.aau.cs.d402f13.utilities.types.Square;
+
+public abstract class SimulatedBoard {
 	protected SimulatedGame game;
 	protected List<SimulatedPieces> pieces = new ArrayList<SimulatedPieces>();
 
 	public SimulatedBoard(SimulatedGame game) {
 		this.game = game;
 	}
+	
+	public abstract Square findSquare(int x, int y);
 
 }
