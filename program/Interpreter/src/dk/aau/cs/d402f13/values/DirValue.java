27cb4fdbbe82786eb7fce40168332f512d9316b7
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2013-04-20 17:03:06 +0200

    Add string addition operations to all values

--- program/Interpreter/src/dk/aau/cs/d402f13/values/DirValue.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/values/DirValue.java
@@ -1,11 +1,17 @@
 package dk.aau.cs.d402f13.values;
 
 import dk.aau.cs.d402f13.utilities.ast.AstNode.Type;
+import dk.aau.cs.d402f13.utilities.errors.StandardError;
 import dk.aau.cs.d402f13.utilities.errors.TypeError;
 
 public class DirValue extends Value {
   private final int x;
   private final int y;
+  
+  public DirValue(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 
   public DirValue(String value) {
     switch(value) {
@@ -56,6 +62,7 @@ public class DirValue extends Value {
     return this.y;
   }
   
+  /** {@inheritDoc}  */
   @Override
   public BoolValue equalsOp(Value other) {
     if(other instanceof DirValue) {
@@ -66,9 +73,49 @@ public class DirValue extends Value {
     }
     return BoolValue.falseValue();
   }
+
+  /** {@inheritDoc}  */
+  @Override
+  public Value add(Value other) throws TypeError {
+    if(other instanceof DirValue) {
+      DirValue oDir = (DirValue)other;   
+      return new DirValue((x + oDir.getX()), y + (oDir.getY()));
+    }
+    else if(other instanceof StrValue)
+      return new StrValue(this.toString() + ((StrValue)other).getValue());
+    throw new TypeError("Addition cannot be done on direction with " + other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public Value negate() throws TypeError {
+    return new DirValue(-x, -y);
+  }
   
   @Override
   public String toString() {
+    return toDir(x, y);
+  }
+  
+  private String toDir(int x, int y) {
+    if((x < 2 && x > -2) && (y < 2 && y > -2)) {
+      if(x == 0 && y == 1)
+        return "n";
+      else if(x == 1 && y == 0)
+        return "e";
+      else if(x == 0 && y == -1)
+        return "s";
+      else if(x == -1 && y == 0)
+        return "w";
+      else if(x == 1 && y == 1)
+        return "ne";
+      else if(x == 1 && y == -1)
+        return "se";
+      else if(x == -1 && y == -1)
+        return "sw";
+      else if(x == -1 && y == 1)
+        return "nw";
+    }
     return "(" + Integer.toString(x) + ", " + Integer.toString(y) + ")";
   }
 }
