c7b056bf825120639dce4c1ced963f25cbbf7675
Niels Poulsen <nielssonnich@gmail.com>
2013-04-21 16:10:46 +0200

    Implicit casting of object

--- program/Interpreter/src/dk/aau/cs/d402f13/values/ObjectValue.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/values/ObjectValue.java
@@ -1,6 +1,8 @@
 package dk.aau.cs.d402f13.values;
 
+import dk.aau.cs.d402f13.interpreter.Interpreter;
 import dk.aau.cs.d402f13.interpreter.Scope;
+import dk.aau.cs.d402f13.utilities.errors.DivideByZeroError;
 import dk.aau.cs.d402f13.utilities.errors.StandardError;
 import dk.aau.cs.d402f13.utilities.errors.TypeError;
 
@@ -21,11 +23,21 @@ public class ObjectValue extends Value {
     this.parent = parent;
   }
 
+  /** {@inheritDoc}  */
   @Override
   public TypeValue getType() {
     return type;
   }
   
+  public Value getRoot() {
+    Value obj = this;
+    while (obj instanceof ObjectValue && ((ObjectValue)obj).parent != null) {
+      obj = ((ObjectValue)obj).parent;
+    }
+    return obj;
+  }
+  
+  /** {@inheritDoc}  */
   @Override
   public Value as(TypeValue type) throws StandardError {
     Value obj = this;
@@ -38,4 +50,103 @@ public class ObjectValue extends Value {
     return obj;
   }
 
+  /** {@inheritDoc}  */
+  @Override
+  public Value call(Interpreter interpreter, Value ... actualParameters) throws StandardError {
+    if (parent != null) {
+      return getRoot().call(interpreter, actualParameters); 
+    }
+    return super.call(interpreter, actualParameters);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public BoolValue lessThan(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().lessThan(other);
+    }
+    return super.lessThan(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public BoolValue lessThanEq(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().lessThanEq(other);
+    }
+    return super.lessThanEq(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public BoolValue greaterThan(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().greaterThan(other);
+    }
+    return super.greaterThan(other);
+  }
+
+  /** {@inheritDoc}  */
+  @Override
+  public BoolValue greaterThanEq(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().greaterThanEq(other);
+    }
+    return super.greaterThanEq(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public Value add(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().add(other);
+    }
+    return super.add(other);
+  }
+
+  /** {@inheritDoc}  */
+  @Override
+  public Value subtract(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().subtract(other);
+    }
+    return super.subtract(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public Value multiply(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().multiply(other);
+    }
+    return super.multiply(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public Value divide(Value other) throws TypeError, DivideByZeroError {
+    if (parent != null) {
+      return getRoot().divide(other);
+    }
+    return super.divide(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public Value mod(Value other) throws TypeError {
+    if (parent != null) {
+      return getRoot().mod(other);
+    }
+    return super.mod(other);
+  }
+  
+  /** {@inheritDoc}  */
+  @Override
+  public Value negate() throws TypeError {
+    if (parent != null) {
+      return getRoot().negate();
+    }
+    return super.negate();
+  }
+  
 }
