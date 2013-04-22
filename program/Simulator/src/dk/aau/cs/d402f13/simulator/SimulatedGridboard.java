9771334399187e33cd3dc7802cd96ea765ba23d5
spillerrec <spillerrec@hotmail.com>
2013-04-22 12:35:06 +0200

    ADD: Mouse dragging of pieces REMOVED: SimulatedPieces, using Piece directly instead

--- program/Simulator/src/dk/aau/cs/d402f13/simulator/SimulatedGridboard.java
+++ program/Simulator/src/dk/aau/cs/d402f13/simulator/SimulatedGridboard.java
@@ -26,52 +26,72 @@ public class SimulatedGridboard extends SimulatedBoard {
 	public SimulatedGridboard( SimulatedGame game, Gridboard b ) {
 		super( game );
 		board = b;
-		for( Piece p : b.getPieces() ){
-			pieces.add( new SimulatedPieces( p ) );
-		}
 	}
 
-	private int pieceXCoordiate( SimulatedPieces p ){
+	private int pieceXCoordiate( Piece p ){
 		return board.squareCoordinateX( p.getSquare() );
 	}
-	private int pieceYCoordiate( SimulatedPieces p ){
+	private int pieceYCoordiate( Piece p ){
 		return board.squareCoordinateY( p.getSquare() );
 	}
 	
+	private Square hoversOn(){
+		if( dragged == null )
+			return null;
+
+		int x = pieceXCoordiate( dragged ) * size + offsetX + dragOffsetX + size / 2;
+		int y = pieceYCoordiate( dragged ) * size + offsetY + dragOffsetY + size / 2;
+		return findSquare( x, y );
+	}
 
 	private void renderBoard( Graphics g, int x, int y, int size, int width, int height) throws SlickException{
-
+		Square hover = hoversOn();
+		
+		//Draw squares
 		for( int iy=0; iy<height; iy++ ){
 			for( int ix=0; ix<width; ix++ ){
 				Square s = board.getSquareAt( ix, iy );
+				int posX = x + ix * size;
+				int posY = y + iy * size;
+				
+				//Draw background for square
 				Image img = game.getImage( s.getImgPath() );
-				
 				int imgMax = Math.max(img.getWidth(), img.getHeight());
-				img.draw( x + ix * size, y + iy * size, (float)size /(float) imgMax );
+				img.draw( posX, posY, (float)size /(float) imgMax );
 				
-				if( s == getSelected() ){
-					g.setColor( new Color(255,0,0,127) );
-					g.fillRect(x + ix * size, y + iy * size, size, size);
+				if( s == selected ){
+					g.setColor( new Color(0,0,127,63) );
+					g.fillRect( posX, posY, size, size);
 				}
 				
 				if( squareIsHinted( s ) ){
-					g.setColor( new Color(0,255,0,127) );
-					g.fillRect(x + ix * size, y + iy * size, size, size);
+					if( s == hover )
+						g.setColor( new Color(0,255,0,191) );
+					else
+						g.setColor( new Color(0,255,0,63) );
+					g.fillRect( posX, posY, size, size);
+				}
+				else{
+					if( s == hover ){
+						g.setColor( new Color(255,0,0,127) );
+						g.fillRect( posX, posY, size, size );
+					}
 				}
 			}
 		}
 	}
 	
-	private void renderPiece( Graphics g, SimulatedPieces p, int x, int y, int size, int offsetX, int offsetY) throws SlickException{
-		p.setImage( game.getImage( p.getImgPath() ) );
+	private void renderPiece( Graphics g, Piece p, int x, int y, int size, int offsetX, int offsetY) throws SlickException{
+		Image img = game.getImage( p.getImgPath() );
+		int imgMax = Math.max( img.getHeight(), img.getWidth() );
 		
 		int borderSize = (int) (size * 0.05);
-		float scale = (size - borderSize * 2) / (float)p.imageSize();
+		float scale = (size - borderSize * 2) / (float)imgMax;
 
-		int imgYOffset = (int) ((p.imageSize() - p.PieceImg.getHeight() ) * scale / 2);
-		int imgXOffset = (int) ((p.imageSize() - p.PieceImg.getWidth() ) * scale / 2);
+		int imgYOffset = (int) ((imgMax - img.getHeight() ) * scale / 2);
+		int imgXOffset = (int) ((imgMax - img.getWidth() ) * scale / 2);
 
-		p.PieceImg.draw( x * size + imgXOffset + offsetX + borderSize, y * size + imgYOffset + offsetY + borderSize, scale);
+		img.draw( x * size + imgXOffset + offsetX + borderSize, y * size + imgYOffset + offsetY + borderSize, scale);
 		
 	}
 	
@@ -94,19 +114,34 @@ public class SimulatedGridboard extends SimulatedBoard {
 		g.fillRect(offsetX - borderWidth, offsetY - borderHeight, size * numSquaresX + borderWidth * 2, size * numSquaresY + borderHeight * 2);
 		renderBoard(g, offsetX, offsetY, size, numSquaresX, numSquaresY);
 		
-		for( SimulatedPieces piece : pieces )
-			renderPiece( g, piece, pieceXCoordiate( piece ), pieceYCoordiate( piece ), size, offsetX, offsetY );
+		for( Piece piece : board.getPieces() )
+			if( piece != dragged )
+				renderPiece( g, piece
+						,	pieceXCoordiate( piece )
+						,	pieceYCoordiate( piece )
+						,	size, offsetX, offsetY
+					);
 		
+		if( dragged != null ){
+			//renderDragged( g, dragged );
+			renderPiece( g, dragged
+					,	pieceXCoordiate( dragged )
+					,	pieceYCoordiate( dragged )
+					,	size, offsetX + dragOffsetX, offsetY + dragOffsetY
+				);
+		}
 	}
 
 	@Override
 	public Square findSquare(int x, int y) {
-		int dx = x - offsetX;
-		int posX = dx / size;
-		int dy = y - offsetY;
-		int posY = dy / size;
-		
-		System.out.println("Sqaure at " +posX +"x" + posY);
+		int posX = (x - offsetX) / size;
+		int posY = (y - offsetY) / size;
 		return board.getSquareAt(posX, posY);
+	}
+	
+	@Override
+	protected void alignDrag(){
+		dragStartX = offsetX + size * ((dragStartX - offsetX) / size) + size / 2;
+		dragStartY = offsetY + size * ((dragStartY - offsetY) / size) + size / 2;
 	}
 }
