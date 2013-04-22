8f8fd624550549beabe128a23f738ac584065cb3
Kent Caspersen <kcaspe11@student.aau.dk>
2013-04-22 15:28:44 +0200

    scopechecker virker nu også på konstanter

--- program/ScopeChecker/src/dk/aau/cs/d402f13/scopechecker/TypeTablePrettyPrinter.java
+++ program/ScopeChecker/src/dk/aau/cs/d402f13/scopechecker/TypeTablePrettyPrinter.java
@@ -17,15 +17,26 @@ public class TypeTablePrettyPrinter {
           System.out.print(", takes " + ci.parentCallArgs + " args");
         }
         System.out.println("");
-        System.out.println("  Has abstract members:");
-        for (Member m : ci.abstractMembers){
+        System.out.println("  Has abstract functions:");
+        for (Member m : ci.abstractFunctions){
           System.out.println("    " + m.name + ", takes " + m.args + " args");
    
         }
          
-        System.out.println("  Has constant members:");
-        for (Member m : ci.concreteMembers){
-          System.out.println("    " + m.name + "takes " + m.args + " args");
+        System.out.println("  Has concrete functions:");
+        for (Member m : ci.concreteFunctions){
+          System.out.println("    " + m.name + ", takes " + m.args + " args");
+          
+        } 
+        System.out.println("  Has abstract constants:");
+        for (Member m : ci.abstractConstants){
+          System.out.println("    " + m.name);
+   
+        }
+         
+        System.out.println("  Has concrete constants:");
+        for (Member m : ci.concreteConstants){
+          System.out.println("    " + m.name);
           
         } 
       }
