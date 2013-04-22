4fc39710807223094f08486c9e4e6f4c82c33c08
Niels Poulsen <nielssonnich@gmail.com>
2013-04-22 15:29:25 +0200

    stdevn += List.filter[] + List.sort[] + List.map[]

--- program/Interpreter/src/dk/aau/cs/d402f13/interpreter/stdenv/StandardEnvironment.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/interpreter/stdenv/StandardEnvironment.java
@@ -2,12 +2,15 @@ package dk.aau.cs.d402f13.interpreter.stdenv;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import dk.aau.cs.d402f13.interpreter.Callable;
 import dk.aau.cs.d402f13.interpreter.ConstantCallable;
 import dk.aau.cs.d402f13.interpreter.Interpreter;
 import dk.aau.cs.d402f13.interpreter.Member;
+import dk.aau.cs.d402f13.interpreter.SortComparator;
 import dk.aau.cs.d402f13.interpreter.SymbolTable;
 import dk.aau.cs.d402f13.utilities.errors.ArgumentError;
 import dk.aau.cs.d402f13.utilities.errors.StandardError;
@@ -61,8 +64,8 @@ public class StandardEnvironment extends SymbolTable {
     ListValue.type().addStaticMember("size", new ConstMemberValue(new ConstantCallable() {
       @Override
       public Value call(Interpreter interpreter, Value object) throws StandardError {
-        ListValue a = (ListValue)object;
-        return new IntValue(a.getValues().length);
+        ListValue o = (ListValue)object;
+        return new IntValue(o.getValues().length);
       }
     }));
     
@@ -70,7 +73,50 @@ public class StandardEnvironment extends SymbolTable {
       @Override
       public Value call(Interpreter interpreter, Value... actualParameters)
           throws StandardError {
-        return interpreter.getSymbolTable().getThis();
+        ListValue o = (ListValue)interpreter.getSymbolTable().getThis();
+        FunValue a = (FunValue)TypeValue.expect(actualParameters, 0, FunValue.type());
+        List<Value> result = new ArrayList<Value>(Arrays.asList(o.getValues()));
+        SortComparator sorter = new SortComparator(interpreter, a);
+        Collections.sort(result, sorter);
+        if (sorter.getError() != null) {
+          throw sorter.getError();
+        }
+        Value[] resultArray = new Value[result.size()];
+        return new ListValue(result.toArray(resultArray));
+      }
+    }));
+    
+    ListValue.type().addStaticMember("map", new ConstMemberValue(1, false, new Callable() {
+      @Override
+      public Value call(Interpreter interpreter, Value... actualParameters)
+          throws StandardError {
+        ListValue o = (ListValue)interpreter.getSymbolTable().getThis();
+        FunValue a = (FunValue)TypeValue.expect(actualParameters, 0, FunValue.type());
+        List<Value> result = new ArrayList<Value>();
+        for (Value v : o.getValues()) {
+          result.add(a.call(interpreter, v));
+        }
+        Value[] resultArray = new Value[result.size()];
+        return new ListValue(result.toArray(resultArray));
+      }
+    }));
+    
+    ListValue.type().addStaticMember("filter", new ConstMemberValue(1, false, new Callable() {
+      @Override
+      public Value call(Interpreter interpreter, Value... actualParameters)
+          throws StandardError {
+        ListValue o = (ListValue)interpreter.getSymbolTable().getThis();
+        FunValue a = (FunValue)TypeValue.expect(actualParameters, 0, FunValue.type());
+        List<Value> result = new ArrayList<Value>();
+        for (Value v : o.getValues()) {
+          Value r = a.call(interpreter, v);
+          BoolValue predicate = (BoolValue)TypeValue.expect(r, BoolValue.type());
+          if (predicate == BoolValue.trueValue()) {
+            result.add(v);
+          }
+        }
+        Value[] resultArray = new Value[result.size()];
+        return new ListValue(result.toArray(resultArray));
       }
     }));
     
@@ -83,6 +129,14 @@ public class StandardEnvironment extends SymbolTable {
     // type: String
     ////////////////////////////////////
     addType("String", StrValue.type());
+    
+    StrValue.type().addStaticMember("size", new ConstMemberValue(new ConstantCallable() {
+      @Override
+      public Value call(Interpreter interpreter, Value object) throws StandardError {
+        StrValue a = (StrValue)object;
+        return new IntValue(a.getValue().length());
+      }
+    }));
     
     ////////////////////////////////////
     // type: Type
@@ -112,7 +166,7 @@ public class StandardEnvironment extends SymbolTable {
     }));
     
     ////////////////////////////////////
-    // Type functions
+    // Global functions
     ////////////////////////////////////
     
     addConstant("typeOf", new FunValue(
@@ -125,21 +179,7 @@ public class StandardEnvironment extends SymbolTable {
         }
       }
     ));
-    
-    ////////////////////////////////////
-    // List functions
-    ////////////////////////////////////
-    addConstant("size", new FunValue(
-      1, false,
-      new Callable() {
-        @Override
-        public Value call(Interpreter interpreter, Value... actualParameters)
-            throws StandardError {
-          ListValue a = (ListValue)TypeValue.expect(actualParameters, 0, ListValue.type());
-          return new IntValue(a.getValues().length);
-        }
-      }
-    ));
+
     addConstant("union", new FunValue(
       1, true,
       new Callable() {
