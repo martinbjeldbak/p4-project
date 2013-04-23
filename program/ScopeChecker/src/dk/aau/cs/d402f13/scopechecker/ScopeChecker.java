51fc7d0e1e5077fff0c3be1b6debb055d2864c3c
Kent Caspersen <kcaspe11@student.aau.dk>
2013-04-23 12:08:14 +0200

    ændret impl. til hashmap <String typeNavn, TypeSymbolInfo tsi> rettet cycle-detection til at foregå i topological sort

--- program/ScopeChecker/src/dk/aau/cs/d402f13/scopechecker/ScopeChecker.java
+++ program/ScopeChecker/src/dk/aau/cs/d402f13/scopechecker/ScopeChecker.java
@@ -1,23 +1,44 @@
 package dk.aau.cs.d402f13.scopechecker;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import dk.aau.cs.d402f13.utilities.ast.AstNode;
 import dk.aau.cs.d402f13.utilities.errors.StandardError;
 import dk.aau.cs.d402f13.utilities.scopechecker.TypeSymbolInfo;
 
 public class ScopeChecker {
-  ArrayList<TypeSymbolInfo> typeTable;
-
+  HashMap<String, TypeSymbolInfo> typeHashMap = new HashMap<String, TypeSymbolInfo>();
+  ArrayList<TypeSymbolInfo> typeList = new ArrayList<TypeSymbolInfo>();
+  //The HashMap and ArraList contains the same elements
+  //Some times it is nice to have a hashmap for quick lookup on type names
+  //In other situations it is nice to have types in a list, for topological sorting e.g.
+  
   public void visit(AstNode node) throws StandardError{
-    TypeVisitor typeVisitor = new TypeVisitor();
+    //Find types, their members, constructor and super constructor call
+    TypeVisitor typeVisitor = new TypeVisitor(); 
     typeVisitor.visit(node);
-    typeTable = typeVisitor.getTypeTable();
+    typeHashMap = typeVisitor.getTypeTable();
+    for (TypeSymbolInfo tsi : typeHashMap.values())
+      typeList.add(tsi);
+    
+    
+    //Make reference from types to their parents, propogate type members to derived types, topological sort types
     TypeTableCleaner ttc = new TypeTableCleaner();
-    typeTable = ttc.clean(typeTable);
-    ttc.markAbstractTypes(typeTable); //any type containing an abstract member is marked as abstract type for use in interpreter
-    TypeExtendErrorChecker teec = new TypeExtendErrorChecker();
-    teec.check(typeTable);
-    TypeTablePrettyPrinter.print(typeTable);
+    typeList = ttc.clean(typeHashMap);
+    
+    //Any type containing at least one abstract member is marked as abstract type for use in interpreter
+    ttc.markAbstractTypes(typeList); 
+    
+    //Check that a call to a parent type constructor has the right number of arguments
+    TypeSuperCallChecker teec = new TypeSuperCallChecker();
+    teec.check(typeList);
+    
+    //Check that members are accessed correct. E.g. in:  $a.b.g.k  member b, g and k must exist in some type
+    TypeMemberAccessVisitor tmav = new TypeMemberAccessVisitor();
+    tmav.setTypeTable(typeHashMap);
+    //tmav.visit(node); not tested yet
+    
+    //TypeTablePrettyPrinter.print(typeTable);
   }
 }
--- program/ScopeChecker/src/dk/aau/cs/d402f13/scopechecker/TypeSuperCallChecker.java
+++ program/ScopeChecker/src/dk/aau/cs/d402f13/scopechecker/TypeSuperCallChecker.java
@@ -5,34 +5,14 @@ import java.util.HashSet;
 import dk.aau.cs.d402f13.utilities.errors.ScopeError;
 import dk.aau.cs.d402f13.utilities.scopechecker.TypeSymbolInfo;
 
-public class TypeExtendErrorChecker {
+public class TypeSuperCallChecker {
   public void check(ArrayList<TypeSymbolInfo> typeTable) throws ScopeError{ //the typeTable is obtained from TypeVisitor and contains the name of all classes, their members (constant and abstract), their constructor args and parent type + parent constructor args
-    checkForCycles(typeTable);  //throw error on extend cycles
     checkSuperCallArgMatches(typeTable); //check that a type's call to a parents constructor matches the arg count
   }
   
- 
-  void checkForCycles(ArrayList<TypeSymbolInfo> typeTable) throws ScopeError{
-    ArrayList<TypeSymbolInfo> types = new ArrayList<TypeSymbolInfo>();
-    for (TypeSymbolInfo ti : typeTable){ //make copy of original list so we dont modify it
-      types.add(ti);
-    }
-   HashSet<TypeSymbolInfo> path = new HashSet<TypeSymbolInfo>();
-   while (types.size() > 0){
-     TypeSymbolInfo current = types.get(0);
-     types.remove(current);
-     path.clear();
-     while (current.parent != null){
-       types.remove(current);
-       path.add(current);
-       current = current.parent;
-       if (path.contains(current))
-         throw new ScopeError("Cycle detected in type inheritance", current);
-     }
-   }
-  }
+  
   void checkSuperCallArgMatches(ArrayList<TypeSymbolInfo> typeTable) throws ScopeError{
-    //ERROR ON: type A[] extends B[$a, $b, $c] {}  if B's constructor needs more or less than 3 args
+    //ERROR WHEN type A[] extends B[$a, $b, $c] {}  if B's constructor argument count does not match
     for (TypeSymbolInfo tsi : typeTable){
       if (tsi.parent != null){
         if (tsi.parentCallArgs != tsi.parent.args){
