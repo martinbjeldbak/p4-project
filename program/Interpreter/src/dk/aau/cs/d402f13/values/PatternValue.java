7dff91d1b08ad75296a3350300af34c36ec2deb1
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2013-04-29 11:43:12 +0200

    Pattern sub-values now extend PatternValue

--- program/Interpreter/src/dk/aau/cs/d402f13/values/PatternValue.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/values/PatternValue.java
@@ -8,15 +8,20 @@ public class PatternValue extends Value {
   private static TypeValue type = new TypeValue("Pattern", 1, false);
 
   @Override
-  public TypeValue getType() {
+  public TypeValue getType() throws StandardError {
     return type;
   }
-  
+
   public static TypeValue type() {
     return type;
   }
 
-  public PatternValue(Value ... patterns) {
+  public PatternValue(Value ... patterns) throws StandardError {
+    for(Value val : patterns) {
+      if(isNotPatTypeCompatible(val))
+        throw new TypeError("The supplied value (" + val + ") cannot be used in patterns");
+    }
+
     this.values = patterns;
   }
 
@@ -37,5 +42,35 @@ public class PatternValue extends Value {
     sb.append(values[values.length-1] + ")");
 
     return sb.toString();
+  }
+
+  protected boolean isPatTypeCompatible(Value v) throws StandardError {
+    if(v.is(IntValue.type()))
+      return true;
+    else if(v.is(StrValue.type()))
+      return true;
+    else if(v.is(DirValue.type()))
+      return true;
+    else if(v.is(CoordValue.type()))
+      return true;
+    else if(v.is(FunValue.type()))
+      return true;
+    else if(v.is(PatternValue.type()))
+      return true;
+    else if(v.is(ListValue.type()))
+      return true;
+    else if(v.is(ActionValue.type()))
+      return true;
+    else if(v.is(PatternMultValue.type()))
+      return true;
+    else if(v.is(PatternOptValue.type()))
+      return true;
+    else if(v.is(PatternPlusValue.type()))
+      return true;
+    return false;
+  }
+
+  protected boolean isNotPatTypeCompatible(Value v) throws StandardError {
+    return !isPatTypeCompatible(v);
   }
 }
