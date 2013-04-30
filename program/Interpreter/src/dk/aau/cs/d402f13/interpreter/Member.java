e91ffb73821044563cf2905fdcf002b9a3522ef3
Niels Poulsen <nielssonnich@gmail.com>
2013-04-30 14:29:19 +0200

    I don't even know what I'm doing anymore

--- program/Interpreter/src/dk/aau/cs/d402f13/interpreter/Member.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/interpreter/Member.java
@@ -2,16 +2,20 @@ package dk.aau.cs.d402f13.interpreter;
 
 import dk.aau.cs.d402f13.utilities.ast.AstNode;
 import dk.aau.cs.d402f13.utilities.errors.StandardError;
-import dk.aau.cs.d402f13.values.ConstMemberValue;
 import dk.aau.cs.d402f13.values.ConstValue;
 import dk.aau.cs.d402f13.values.FunValue;
+import dk.aau.cs.d402f13.values.ObjectValue;
 import dk.aau.cs.d402f13.values.Value;
 
 public class Member {
   private AstNode params;
   private AstNode expression;
   
-  private ConstMemberValue constValue;
+  private ConstantCallable constant;
+  
+  private int minArity;
+  private boolean varArgs;
+  private Callable function;
   
   public Member(AstNode definition) {
     expression = definition.getLast();
@@ -21,13 +25,22 @@ public class Member {
     }
   }
   
-  public Member(ConstMemberValue constValue) {
-    this.constValue = constValue;
+  public Member(ConstantCallable constant) {
+    this.constant = constant;
+  }
+
+  public Member(int minArity, boolean varArgs, Callable function) {
+    this.minArity = minArity;
+    this.varArgs = varArgs;
+    this.function = function;
   }
   
   public Value getValue(Interpreter interpreter, Scope scope) throws StandardError {
-    if (constValue != null) {
-      return constValue.evaluate(interpreter, scope.getThis());
+    if (constant != null) {
+      interpreter.getSymbolTable().openScope(scope);
+      Value ret = constant.call(interpreter, scope.getThis());
+      interpreter.getSymbolTable().closeScope();
+      return ret;
     }
     if (params == null) {
       interpreter.getSymbolTable().openScope(scope);
@@ -35,10 +48,20 @@ public class Member {
       interpreter.getSymbolTable().closeScope();
       return ret;
     }
+    if (function != null) {
+      return new FunValue(minArity, varArgs, function, scope);
+    }
     return new FunValue(params, expression, scope);
   }
   
   public Value getValue(Interpreter interpreter) throws StandardError {
     return getValue(interpreter, interpreter.getSymbolTable().currentScope());
+  }
+  
+  public Value getValue(Interpreter interpreter, Value value) throws StandardError {
+    if (value instanceof ObjectValue) {
+      return getValue(interpreter, ((ObjectValue)value).getScope());
+    }
+    return getValue(interpreter, new Scope(value));
   }
 }
