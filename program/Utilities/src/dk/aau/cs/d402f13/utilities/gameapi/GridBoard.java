5a1c47dd82b51c0dc16f0fb7f085ca1398cd289a
Niels Poulsen <nielssonnich@gmail.com>
2013-05-03 10:45:34 +0200

    Useless Board-interface

--- program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/GridBoard.java
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/GridBoard.java
@@ -2,8 +2,7 @@ package dk.aau.cs.d402f13.utilities.gameapi;
 
 import dk.aau.cs.d402f13.utilities.errors.StandardError;
 
-public interface Board {
-  public Piece[] getPieces() throws StandardError;
+public interface GridBoard extends Board {
   public Square[] getSquares() throws StandardError;
   public Square[] getEmptySquares() throws StandardError;
   public Square getSquareAt(int x, int y) throws StandardError;
