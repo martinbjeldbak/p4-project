67d1b0b9916ad5cf98c670dedb031120a2b37541
Niels Poulsen <nielssonnich@gmail.com>
2013-05-03 14:08:13 +0200

    Pieces now know where they are

--- program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/Piece.java
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/Piece.java
@@ -6,5 +6,7 @@ import dk.aau.cs.d402f13.utilities.types.Action;
 public interface Piece {
   public String getImage() throws StandardError;
   public Player getOwner() throws StandardError;
+  public Square getSquare() throws StandardError;
+  public boolean isOnBoard() throws StandardError;
   public Action[] getActions() throws StandardError;
 }
