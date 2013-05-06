9287b6220e2b9b37942a2eb2240176aea8414f9b
Niels Poulsen <nielssonnich@gmail.com>
2013-05-06 11:11:39 +0200

    Remove old actions from interfaces

--- program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/Player.java
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/Player.java
@@ -1,7 +1,6 @@
 package dk.aau.cs.d402f13.utilities.gameapi;
 
 import dk.aau.cs.d402f13.utilities.errors.StandardError;
-import dk.aau.cs.d402f13.utilities.types.Action;
 
 public interface Player {
   public boolean winCondition(Game game) throws StandardError;
