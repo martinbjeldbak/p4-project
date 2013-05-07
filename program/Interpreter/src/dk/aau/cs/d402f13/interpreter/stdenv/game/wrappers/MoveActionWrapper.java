61667d1be0dbfa32b8986da2258383f47f5506f1
Niels Poulsen <nielssonnich@gmail.com>
2013-05-07 12:13:01 +0200

    Remove from()

--- program/Interpreter/src/dk/aau/cs/d402f13/interpreter/stdenv/game/wrappers/MoveActionWrapper.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/interpreter/stdenv/game/wrappers/MoveActionWrapper.java
@@ -10,22 +10,15 @@ import dk.aau.cs.d402f13.values.Value;
 public class MoveActionWrapper extends UnitActionWrapper implements MoveAction {
 
   private SquareWrapper to;
-  private SquareWrapper from;
   
   public MoveActionWrapper(GameEnvironment env, Value object) throws StandardError {
     super(env, object);
     to = new SquareWrapper(env, getMember("to", env.squareType()));
-    from = new SquareWrapper(env, getMember("from", env.squareType()));
   }
 
   @Override
   public SquareWrapper getTo() throws StandardError {
     return to;
-  }
-
-  @Override
-  public SquareWrapper getFrom() throws StandardError {
-    return from;
   }
  
 
