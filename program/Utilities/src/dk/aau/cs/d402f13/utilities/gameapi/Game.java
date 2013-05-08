6bb17022891eb23f0d2db304c49245da57d4ba94
Niels Poulsen <nielssonnich@gmail.com>
2013-05-08 14:11:53 +0200

    Add description to game

--- program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/Game.java
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/gameapi/Game.java
@@ -4,6 +4,7 @@ import dk.aau.cs.d402f13.utilities.errors.StandardError;
 
 public interface Game {
   public String getTitle() throws StandardError;
+  public String getDescription() throws StandardError;
   public Board getBoard() throws StandardError;
   public Player getCurrentPlayer() throws StandardError;
   public Player[] getPlayers() throws StandardError;
