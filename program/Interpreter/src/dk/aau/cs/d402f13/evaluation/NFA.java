f95ca38d633ae5b894ad71c6171264cdc23d44b6
Martin Madsen <martin2madsen@gmail.com>
2013-05-09 15:10:42 +0200

    Add to dot method to NFAs

--- program/Interpreter/src/dk/aau/cs/d402f13/evaluation/NFA.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/evaluation/NFA.java
@@ -3,12 +3,18 @@ package dk.aau.cs.d402f13.evaluation;
 import dk.aau.cs.d402f13.values.PatternValue;
 import dk.aau.cs.d402f13.values.Value;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class NFA {
   private State entry;
   private ArrayList<State> exit = new ArrayList<>();
-  private static PatternValue pattern = null;
 
   /**
    * Sets the entry point in this NFA.
@@ -113,7 +119,7 @@ public class NFA {
   /**
    * Creates an NFA which which matches zero-or-more
    * repetitions of the given NFA. Also known as the
-   * kleene star operation ('*').
+   * kleene star regular expression operation ('*').
    * @param nfa the NFA to add a kleene star to
    * @return    the NFA with the kleene star operator
    */
@@ -128,6 +134,14 @@ public class NFA {
     return new NFA(entry, nfa.getExit());
   }
 
+  /**
+   * Creates an NFA which matches one-to-many
+   * repetitions of the given NFA. The
+   * ('+') regular expression operation.
+   * @param nfa the NFA to add a plus operation
+   *            on
+   * @return    the NFA with the plus operation
+   */
   public static final NFA plus(NFA nfa) {
     for(State exit : nfa.getExit())
       exit.addEmptyEdge(nfa.getEntry());
@@ -153,6 +167,16 @@ public class NFA {
     }
 
     return new NFA(first.getEntry(), second.getExit());
+  }
+
+  /** Instance method identical to {@link #concat(NFA, NFA)},
+   * but uses the current NFA instead */
+  public final NFA concat(NFA second) {
+    for (State s : this.getExit()) {
+      s.setAccept(false);
+      s.addEmptyEdge(second.getEntry());
+    }
+    return new NFA(this.getEntry(), second.getEntry());
   }
 
   /**
@@ -196,5 +220,73 @@ public class NFA {
     NFA exp = e();
 
     return null;
+  }
+
+  public final void toDot() {
+    Path file = createFile("NFA.dot");
+
+    try(BufferedWriter writer = Files.newBufferedWriter(file, Charset.defaultCharset())) {
+      writeLine("digraph NFA {", writer);
+
+      writeLine("rankdir=LR;", writer);
+
+      makeEdges(entry, 0, writer);
+
+      writeLine("}", writer);
+      writer.close();
+    }
+    catch(IOException ex) {
+      System.out.println("Error writing to file");
+    }
+  }
+
+  private void makeEdges(State s, Integer nodeNr, BufferedWriter writer) {
+    int sID = s.hashCode();
+    int eID;
+
+    writeLine(sID + label(""+nodeNr) + ";", writer);
+
+    // For every valued edge leaving the state, print dot language
+    for(Map.Entry<Value, State> edge : s.getValueEdges().entrySet()) {
+      eID = edge.getValue().hashCode();
+
+      writeLine(sID + " -> " + eID + label(edge.getKey().toString()) + ";", writer);
+
+      // Then for this state, make edges for its edges to other states recursively
+      makeEdges(edge.getValue(), nodeNr + 1, writer);
+    }
+
+    // For every epsilon edge, do the same
+    for(State eps : s.getEmptyEdges()) {
+      eID = eps.hashCode();
+
+      writeLine(sID + " -> " + eID + ";", writer);
+      makeEdges(eps, nodeNr + 1, writer);
+    }
+  }
+
+  private void writeLine(CharSequence s, BufferedWriter writer) {
+    try {
+      writer.append(s);
+      writer.newLine();
+    } catch (IOException e) {
+      System.out.print("Failed to write line");
+    }
+
+  }
+
+  private String label(String label) {
+    return " [label=\"" + label + "\"]";
+  }
+
+  private Path createFile(String fileName) {
+    Path file = Paths.get(fileName);
+    try {
+      Files.deleteIfExists(file);
+      file = Files.createFile(file);
+    } catch (IOException e) {
+      System.out.println("Error creating file");
+    }
+    return file;
   }
 }
