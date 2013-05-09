a30f44a9be4d7666459c963e474df13077f217d5
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2013-05-09 13:02:11 +0200

    Create an NFA based on a pattern. Time to clean up the code.

--- program/Interpreter/src/dk/aau/cs/d402f13/evaluation/GridBoardPatternEvaluator.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/evaluation/GridBoardPatternEvaluator.java
@@ -1,12 +1,7 @@
 package dk.aau.cs.d402f13.evaluation;
 
-import dk.aau.cs.d402f13.utilities.gameapi.GridBoard;
-import dk.aau.cs.d402f13.utilities.types.Board;
 import dk.aau.cs.d402f13.utilities.types.Game;
-import dk.aau.cs.d402f13.utilities.types.Piece;
-import dk.aau.cs.d402f13.utilities.types.Player;
-import dk.aau.cs.d402f13.values.CoordValue;
-import dk.aau.cs.d402f13.values.PatternValue;
+import dk.aau.cs.d402f13.values.*;
 
 import java.util.List;
 
@@ -21,23 +16,38 @@ public class GridBoardPatternEvaluator {
     this.game = game;
   }
 
+  public GridBoardPatternEvaluator(PatternValue pattern) {
+    this.pattern = pattern;
+    this.coord = null;
+    this.game = null;
+  }
+
   public boolean evaluate() {
-
-    // Move this stuff in separate, private method
-    List<Piece> pieces = game.board().getPieces();
-
-    for(Piece piece : pieces) {
-      if(piece.player() == game.currentPlayer()) {
-        // Check if the piece's coordinate is the same as the supplied coordinate
-      }
-    }
-
-
-    if(game.board() instanceof GridBoard) {
-
-    }
-
+    NFA nfa = createNFA(NFA.e(), pattern);
 
     return false;
+  }
+
+  private NFA createNFA(NFA nfa, Value v) {
+
+    if(v instanceof PatternOrValue) {
+      Value left = ((PatternOrValue) v).getLeft();
+      Value right = ((PatternOrValue) v).getRight();
+
+      return NFA.concat(nfa, NFA.union(createNFA(nfa, left), createNFA(nfa, right)));
+    }
+    else if(v instanceof PatternMultValue) {
+      return NFA.concat(nfa, NFA.kleeneStar(createNFA(nfa, ((PatternMultValue) v).getValue())));
+    }
+    else if(v instanceof PatternPlusValue) {
+      return NFA.concat(nfa, NFA.plus(createNFA(nfa, ((PatternPlusValue) v).getValue())));
+    }
+    else if(v instanceof PatternValue) {
+      for(Value val : ((PatternValue) v).getValues())
+        return NFA.concat(nfa, createNFA(nfa, val));
+    }
+    // If it's just a value, return an NFA
+    // with the value as a label
+    return NFA.v(v);
   }
 }
