9b972882aa6c5e406ecb24d28126376631aaa339
Martin Madsen <martin2madsen@gmail.com>
2013-05-13 16:11:28 +0200

    Add concatentation to new NFA

--- program/Interpreter/src/dk/aau/cs/d402f13/evaluation/NFA2.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/evaluation/NFA2.java
@@ -12,12 +12,12 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class NFA2 {
-  private State startState;
-  private ArrayList<State> states = new ArrayList<State>();
-  private ArrayList<State> acceptStates = new ArrayList<State>();
+  private State2 startState;
+  private ArrayList<State2> states = new ArrayList<State2>();
+  private ArrayList<State2> acceptStates = new ArrayList<State2>();
   private ArrayList<Transition> transitions = new ArrayList<Transition>();
   
-  public NFA2(State startState, ArrayList<State> states, ArrayList<State> acceptStates, ArrayList<Transition> transitions){
+  public NFA2(State startState, ArrayList<State2> states, ArrayList<State2> acceptStates, ArrayList<Transition> transitions){
     this.startState = startState;
     this.states = states;
     this.acceptStates = acceptStates;
@@ -25,12 +25,22 @@ public class NFA2 {
   }
   
   public void not(){
-    ArrayList<State> newAccept = new ArrayList<State>();
-    for (State s : this.states){
+    ArrayList<State2> newAccept = new ArrayList<State2>();
+    for (State2 s : this.states){
       if (!this.acceptStates.contains(s))
         newAccept.add(s);
     }
     this.acceptStates = newAccept;
   }
-  
+
+  public void concat(NFA2 nfa2) {
+    for(State2 s : this.acceptStates) {
+      this.transitions.add(new Transition(s, nfa2.startState, null));
+    }
+
+    this.states.addAll(nfa2.states);
+    this.transitions.addAll(nfa2.transitions);
+    this.acceptStates = nfa2.acceptStates;
+  }
+ 
 }
