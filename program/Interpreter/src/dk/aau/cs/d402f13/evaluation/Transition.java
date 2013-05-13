25c1f10f7c1d9d317ee66c0b263f6f07f9187867
Martin Madsen <martin2madsen@gmail.com>
2013-05-13 17:37:54 +0200

    Update with javadoc

--- program/Interpreter/src/dk/aau/cs/d402f13/evaluation/Transition.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/evaluation/Transition.java
@@ -2,13 +2,10 @@ package dk.aau.cs.d402f13.evaluation;
 
 import dk.aau.cs.d402f13.values.Value;
 
-import java.util.*;
-import java.util.ArrayList;
-
 public class Transition {
- State2 from, to;
+ State from, to;
  Value val;
-  public Transition(State2 from, State2 to, Value val){
+  public Transition(State from, State to, Value val){
     this.from = from;
     this.to = to;
     this.val = val;
