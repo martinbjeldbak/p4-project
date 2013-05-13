31e52b95d494bc43e6463763ca7c46f81f673332
Martin Madsen <martin2madsen@gmail.com>
2013-05-13 20:17:13 +0200

    Create NFAs for pattern, not, and or

--- program/Interpreter/src/dk/aau/cs/d402f13/evaluation/GridBoardPatternEvaluator.java
+++ program/Interpreter/src/dk/aau/cs/d402f13/evaluation/GridBoardPatternEvaluator.java
@@ -21,27 +21,78 @@ public class GridBoardPatternEvaluator {
   }
 
   public boolean evaluate() {
-    OldNFA nfa = createNFA(OldNFA.e(), pattern);
-    nfa.toDot();
+    //OldNFA nfa = createOldNFA(OldNFA.e(), pattern);
+    //nfa.toDot();
 
-    //System.out.println(nfa.getEntry());
+    NFA nfa = new NFA();
+
+    createNFA(nfa, pattern);
+    nfa.toDot();
 
     return false;
   }
 
-  private OldNFA createNFA(OldNFA nfa, Value v) {
+  private void createNFA(NFA n, Value v) {
+    if(v instanceof PatternKeyValue) {
+      PatternKeyValue val = (PatternKeyValue)v;
+
+      n.concat(new NFA(val));
+    }
+    else if(v instanceof PatternNotValue) {
+      PatternNotValue val = (PatternNotValue)v;
+
+      // Create the NFA for the value, then not the NFA
+      NFA not = new NFA();
+      createNFA(not, val.getValue());
+      not.not();
+
+      // Finally, add this NFA to the current NFA
+      n.concat(not);
+    }
+    else if(v instanceof PatternOrValue) {
+      PatternOrValue val = (PatternOrValue)v;
+
+      NFA opt1 = new NFA();
+      NFA opt2 = new NFA();
+
+      createNFA(opt1, val.getLeft());
+      createNFA(opt2, val.getRight());
+
+      opt1.union(opt2);
+
+      n.concat(opt1);
+    }
+    else if(v instanceof PatternOptValue) {
+
+    }
+    else if(v instanceof PatternMultValue) {
+
+    }
+    else if(v instanceof PatternPlusValue) {
+
+    }
+    else if(v instanceof PatternValue) {
+       for(Value val : ((PatternValue) v).getValues()) {
+         createNFA(n, val);
+       }
+    }
+    else
+      n.concat(new NFA(v));
+  }
+
+  private OldNFA createOldNFA(OldNFA nfa, Value v) {
 
     if(v instanceof PatternOrValue) {
       Value left = ((PatternOrValue) v).getLeft();
       Value right = ((PatternOrValue) v).getRight();
 
-      return OldNFA.union(createNFA(nfa, left), createNFA(nfa, right));
+      return OldNFA.union(createOldNFA(nfa, left), createOldNFA(nfa, right));
     }
     else if(v instanceof PatternMultValue) {
-      return OldNFA.kleeneStar(createNFA(nfa, ((PatternMultValue) v).getValue()));
+      return OldNFA.kleeneStar(createOldNFA(nfa, ((PatternMultValue) v).getValue()));
     }
     else if(v instanceof PatternPlusValue) {
-      return OldNFA.plus(createNFA(nfa, ((PatternPlusValue) v).getValue()));
+      return OldNFA.plus(createOldNFA(nfa, ((PatternPlusValue) v).getValue()));
     }
     else if(v instanceof PatternKeyValue) {
       return OldNFA.v(v);
@@ -57,7 +108,7 @@ public class GridBoardPatternEvaluator {
       OldNFA cur = nfa;
 
       for(Value val : ((PatternValue) v).getValues()) {
-        cur = OldNFA.concat(cur, createNFA(cur, val));
+        cur = OldNFA.concat(cur, createOldNFA(cur, val));
       }
 
       return cur;
