98734b959c67dd15c65ba602da018676cb33bb1d
Kent Caspersen <kcaspe11@student.aau.dk>
2013-05-19 17:50:21 +0200

    PatternCheck er udmiddelbart færdigt, men skal lige checkes efter

--- program/Utilities/src/dk/aau/cs/d402f13/utilities/SimpleDir.java
+++ program/Utilities/src/dk/aau/cs/d402f13/utilities/SimpleDir.java
@@ -1,5 +1,7 @@
 package dk.aau.cs.d402f13.utilities;
 
+import dk.aau.cs.d402f13.utilities.scopechecker.SymbolInfo;
+
 public class SimpleDir {
   public int x, y;
   public SimpleDir(int x, int y){
@@ -12,4 +14,24 @@ public class SimpleDir {
   public SimpleDir clone(){
     return new SimpleDir(this.x, this.y);
   }
+  @Override
+  public int hashCode() {
+    return this.x + this.y * 119;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+      if (obj == null)
+          return false;
+      if (obj == this)
+          return true;
+      if (obj.getClass() != getClass())
+          return false;
+  
+      SimpleDir other = (SimpleDir) obj;
+      if (other.x == this.x && other.y == this.y)
+        return true;
+      return false;
+  }
+  
 }
